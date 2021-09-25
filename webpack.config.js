2d5b3a0e42fe13f79324b969a6b842dd162f09a0
samba <samba@manjaro>
2021-09-25 18:15:48 +0200

    changed library type to umd

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    library: "WmDbg",
+    library: {
+      name: 'WmDbg',
+      type: 'umd'
+    }
   },
 };

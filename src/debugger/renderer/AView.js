8164050d2215e896be9f9fa864617b3abbe652c3
samba <samba@manjaro>
2022-05-16 18:06:06 +0200

    impl. findEventIndices

--- src/debugger/renderer/AView.js
+++ src/debugger/renderer/AView.js
@@ -1,3 +1,5 @@
+import * as _ from 'lodash';
+
 export class AView {
     element = null;
     /**
@@ -48,6 +50,22 @@ export class AView {
             return null;
         }
         return element;
+    }
+
+    findEventIndices(element) {
+        for(let trackIndex = 0; trackIndex < this.eventToElementMap.length; ++trackIndex) {
+            const eventElements = this.eventToElementMap[trackIndex];
+            if (!eventElements) {
+                continue;
+            }
+            for(let eventIndex = 0; eventIndex < eventElements.length; ++eventIndex) {
+                const elementToCheck = eventElements[eventIndex];
+                if (element === elementToCheck) {
+                    return {trackIndex, eventIndex};
+                }
+            }
+        }
+        return null;
     }
     
     /**
